ba10b800911d3267370ef1d03ac1b3ea5d48bc50
deeep8250 <deepmondal825005@gmail.com>
2024-09-02 20:48:23 +0530

    random picture after reopen

--- lib/gpt.dart
+++ lib/gpt.dart
@@ -1,89 +1,60 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
 
-void main() => runApp(RandomImagesApp());
+void main() => runApp(randompositionS());
 
-class RandomImagesApp extends StatelessWidget {
+
+
+class randompositionS extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: RandomImagesScreen(),
-     );
-  }
-}
-
-class RandomImagesScreen extends StatefulWidget {
-  @override
-  _RandomImagesScreenState createState() => _RandomImagesScreenState();
-
-                                               }
-
-class _RandomImagesScreenState extends State<RandomImagesScreen> {
-  final int imageCount = 2;
-  final List<String> imagePaths = [
-    'images/setting.png',
-    'images/img.png',
-    // 'images/image3.png',
-    // 'images/image4.png',
-    // 'images/image5.png',
-    // 'images/image6.png',
-    // 'images/image7.png',
-    // 'images/image8.png',
-    // 'images/image9.png',
-    // 'images/image10.png',
-
-      ];
-  late List<Offset> positions;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _generateRandomPositions();
-  }
-
-  void _generateRandomPositions() {
-    Random random = Random();
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-
-    positions = List.generate(imageCount, (_) {
-      double x = random.nextDouble() * (screenWidth - 50); // Subtract image width
-      double y = random.nextDouble() * (screenHeight - 50); // Subtract image height
-      return Offset(x, y);
-    }
+      home: randomposition(),
     );
   }
+
+}
+
+
+class randomposition extends StatefulWidget {
+
+  State<StatefulWidget> createState() => _random();
+}
+
+class _random extends State {
+
+  List<String> image = [
+    'images/logo.jpg',
+    'images/feedback.png',
+    'images/order history.png',
+    'images/setting.png',
+    'images/sign out.png',
+    'images/img.png',
+    'images/card.jpeg',
+  ];
+   List <String> func () {
+     List<String> random = List.from(image);
+       random.shuffle();
+        return random;
+    }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: List.generate(imageCount, (index) {
-          return Positioned(
-            left: positions[index].dx,
-            top: positions[index].dy,
-            child: InkWell(
-              onTap: () {
-                // Define your tap action here
-              },
-              child: Image.asset(
-                imagePaths[index],
-                width: 50, // Adjust width to fit your image
-                height: 50, // Adjust height to fit your image
-              ),
-            ),
-          );
+       final shuffleList = func();
+       return Scaffold(body: GridView.builder(
+                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                   ),
+                   itemCount: shuffleList.length,
+                   itemBuilder: (context , index){
+                          return Container(
+                                  margin: EdgeInsets.all(12),
+                                  color: Colors.black38,
+                            child: Image.asset(shuffleList[index]),
+                          );
 
-        }
-        ),
-      ),
-
-    );
+                   }
+       ),
+       );
   }
 }
